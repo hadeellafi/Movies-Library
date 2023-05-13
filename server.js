9390a1b52b20025280df2e5cc54ee6978619827f
hadeellafi <hadeellafi9@gmail.com>
2023-05-13 10:50:21 +0000

    lab17

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const server = express();
 require('dotenv').config();
 
 server.use(cors());
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 const axios = require('axios');
 
 const apiKey = process.env.APIkey;
